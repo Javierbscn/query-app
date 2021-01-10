936cede65a5975d6f364ac90826cb7f63b1dac9f
JavierBoscan <javierboscan16@gmail.com>
2021-01-09 22:03:17 -0400

    creando metodos y propiedades para mostrar los datos

--- src/app/components/all-business/all-business.component.ts
+++ src/app/components/all-business/all-business.component.ts
@@ -8,12 +8,62 @@ import { Sale } from './../../models/sale';
   styleUrls: ['./all-business.component.css'],
 })
 export class AllBusinessComponent implements OnInit {
+  agencyMostSales: any;
   sales: Sale[];
-  salesOrdered: Sale[];
-  constructor(public salesService: SalesService) {}
+  orderedSales: Sale[];
+  symbolAgencys: string;
+  symbolSales: string;
+  constructor(public salesService: SalesService) {
+    this.symbolAgencys = '▼';
+    this.symbolSales = '▼';
+  }
 
   ngOnInit(): void {
-    this.salesService.getSales().subscribe((data) => this.sales = data);
+    this.salesService.getSales().subscribe((data) => {
+      this.sales = data;
+      this.orderedSales = this.sales;
+      this.agencyMostSales = this.sales.reduce((acc, doc) => acc.finalPrice > doc.finalPrice ? acc : doc)
+      this.agencyMostSales = this.getAgencyMostSales(this.agencyMostSales)
+    });
+  }
+
+  getAgencyMostSales(agencyMostSales: Sale) {
+    const name = agencyMostSales.nameAgency;
+    let totalSales = 0
+    this.sales.forEach(doc => {
+      if (doc.nameAgency === name) {
+        totalSales += doc.finalPrice
+      }
+    });
+    return { name, totalSales }
+  }
+
+  sortByAgencys() {
+    if (this.orderedSales[0].nameAgency[0].toUpperCase() !== 'A') {
+      this.orderedSales = this.orderedSales.sort((a: Sale, b: Sale) => {
+        this.symbolAgencys = '▼';
+        return a.nameAgency > b.nameAgency ? 1 : 0;
+      });
+    } else {
+      this.orderedSales = this.orderedSales.sort((a: Sale, b: Sale) => {
+        this.symbolAgencys = '▲';
+        return a.nameAgency < b.nameAgency ? 1 : 0;
+      });
+    }
+  }
+
+  sortBySales() {
+    if (this.orderedSales[0].finalPrice > 500) {
+      this.orderedSales = this.orderedSales.sort((a: Sale, b: Sale) => {
+        this.symbolSales = '▼';
+        return a.finalPrice > b.finalPrice ? 1 : 0;
+      });
+    } else {
+      this.orderedSales = this.orderedSales.sort((a: Sale, b: Sale) => {
+        this.symbolSales = '▲';
+        return a.finalPrice > b.finalPrice ? 0 : 1;
+      });
+    }
   }
 
   getData() {}
