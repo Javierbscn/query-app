e817943c7f526f9bb240699efe6223516d0af89d
JavierBoscan <javierboscan16@gmail.com>
2021-01-10 17:01:30 -0400

    configurando routing y agregando HomeComponent y NotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from "@angular/fire";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { AllBusinessComponent } from './components/all-business/all-business.component';
 import { OneBusinessComponent } from './components/one-business/one-business.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
 import { SalesService } from './services/sales.service';
 
 import { environment } from "./../environments/environment";
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'empresas', component: AllBusinessComponent },
+  { path: 'empresas/:nombre_empresa', component: OneBusinessComponent },
+  { path: '**', component: NotFoundComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
     AllBusinessComponent,
-    OneBusinessComponent
+    OneBusinessComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [SalesService],
